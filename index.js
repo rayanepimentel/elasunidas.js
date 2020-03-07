d7526994a77b71f27578faa288a7c51413a99f6b
rayanepimentel <radharayg@gmail.com>
2020-03-07 16:27:37 -0300

    passo 01

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert('Olá Mundo!');
+//alert('Olá Mundo!');
+
+/*
+- Perguntar o nome do usuário; criar uma variável
+- Retornar mensagem de "Bem-vinda"; Retornar alert()
+- Essa mensagem precisa ser personalizada, com o nome do usuário.
+*/
+
+let nome = prompt('Olá, qual é o seu nome?');
+
+//para sabermos se estamos 'salvando' o valor da variável. Ultilizamos o console.log()
+
+console.log(nome)
+
+alert("Seja bem-vinda! " + nome)
